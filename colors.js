938457280a619f9c6d788c259cd3539493ef2499
michaelbzhu <mbzhu1@gmail.com>
2021-01-17 22:56:10 -0800

    initial setup completed

---
+++ colors.js
@@ -0,0 +1,41 @@
+// get the system theme media query and localStorage theme
+let systemIsDark = window.matchMedia('(prefers-color-scheme: dark)');
+let theme = localStorage.getItem('mbz-color-mode');
+
+// set color model to dark mode
+function darkMode() {
+  document.documentElement.setAttribute('theme', 'dark');
+  localStorage.setItem('mbz-color-mode', 'dark');
+}
+
+// set color mode to light mode
+function lightMode() {
+  document.documentElement.setAttribute('theme', 'light');
+  localStorage.setItem('mbz-color-mode', 'light');
+}
+
+// toggle the color mode
+function toggleColorMode() {
+  let theme = localStorage.getItem('mbz-color-mode');
+
+  if (theme === 'dark') {
+    lightMode();
+  } else if (theme === 'light') {
+    darkMode();
+  } else if (systemIsDark.matches) {
+    lightMode();
+  } else {
+    darkMode();
+  }
+}
+
+// default color mode on reload
+if (theme === 'dark') {
+  darkMode();
+} else if (theme === 'light') {
+  lightMode();
+} else if (systemIsDark.matches) {
+  darkMode();
+} else {
+  lightMode();
+}
