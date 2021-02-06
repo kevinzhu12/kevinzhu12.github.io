0f08b0ae21c809a7b9b2ad97f233f28c86cba75a
michaelbzhu <mbzhu1@gmail.com>
2021-02-06 00:17:55 -0800

    text mode toggle

---
+++ textmode.js
@@ -0,0 +1,46 @@
+let mode = localStorage.getItem('mbz-view-mode');
+md8 = document.querySelectorAll('.col-md-8');
+console.log(md8);
+
+text_mode_button = document.querySelector('#text-mode-toggle');
+
+function setTextMode() {
+  text_mode_button.innerHTML = 'Image Mode';
+  document.documentElement.setAttribute('mode', 'text');
+  localStorage.setItem('mbz-view-mode', 'text');
+  for (var i = 0; i < md8.length; i++) {
+    md8[i].classList.remove('col-md-8');
+    console.log(md8[i]);
+  }
+}
+
+function setImageMode() {
+  text_mode_button.innerHTML = 'Text Mode';
+  document.documentElement.setAttribute('mode', 'image');
+  localStorage.setItem('mbz-view-mode', 'image');
+  for (var i = 0; i < md8.length; i++) {
+    md8[i].classList.add('col-md-8');
+    console.log(md8[i]);
+  }
+}
+
+if (mode === 'image') {
+  setImageMode();
+} else if (mode === 'text') {
+  setTextMode();
+} else {
+  setImageMode();
+}
+
+function toggleTextMode() {
+  let mode = localStorage.getItem('mbz-view-mode');
+  if (mode === 'text') {
+    setImageMode();
+  } else if (mode === 'image') {
+    setTextMode();
+  } else {
+    setImageMode();
+  }
+}
+
+text_mode_button.onclick = toggleTextMode;
